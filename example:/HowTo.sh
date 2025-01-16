2782d89f732ee5f8e7603df774a235a071eaed59
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-01-16 01:52:12 -0600

    Create HowTo.sh

---
+++ example:/HowTo.sh
@@ -0,0 +1,10 @@
+git clone https://gitlab.com/newbit/rootAVD.git
+
+cd rootAVD
+
+./rootAVD.sh ListAllAVDs
+
+# example, change to match yours from above List:
+./rootAVD.sh system-images/android-35/google_apis/x86_64/ramdisk.img
+
+# Magisk should now be in your Apps!
